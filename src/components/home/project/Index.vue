c3194bb8a5bc201e0f3283ae2df055e31955909d
nx18668161482 <1138659665@qq.com>
2020-09-09 13:55:13 +0800

    update

---
+++ src/components/home/project/Index.vue
@@ -0,0 +1,162 @@
+<template>
+  <el-col :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
+    <div class="nice-card">
+      <div class="nice-card-header">
+        <span class="label flex-row"> <i class="iconfont nice-icon-manage"></i> 我的项目</span>
+        <el-link type="primary" :underline="false" style="font-size: 13px;"><img
+            src="../../../assets/images/contact/github.png" alt=""></el-link>
+      </div>
+      <div class="nice-card-bodyer">
+        <el-row :gutter="10">
+          <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="8" v-for="item of github" :key="item.id">
+            <div class="project-card">
+              <div class="bg-cover">
+                <div class="bg-effect"
+                  style="background-image: url('https://resources.ninghao.org/images/gravity-falls.png');">
+                  <span class="layer"></span>
+                </div>
+                <div class="info">
+                  <h3 class="flex-between">{{ item.name }} <small class="flex-row"><i
+                        class="el-icon-star-on"></i>{{item.stargazers_count}}</small> </h3>
+                  <p>{{ item.description }}</p>
+                </div>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </el-col>
+</template>
+
+<script>
+  import github from '@/utils/api.js'
+  export default {
+    data() {
+      return {
+        github
+      };
+    },
+    components: {},
+    computed: {},
+    watch: {},
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+  }
+</script>
+<style lang='scss' scoped>
+  .nice-card {
+    position: relative;
+
+    .nice-card-header {
+      .label {
+        flex: 1;
+
+        i {
+          font-size: 20px;
+          margin-right: 5px;
+        }
+      }
+    }
+
+    .nice-card-bodyer {
+      padding-top: 0;
+    }
+
+    .project-card {
+      border-radius: 4px;
+      margin-bottom: 10px;
+      transition: all 0.4s;
+      position: relative;
+
+      .bg-cover {
+        width: 100%;
+        height: 100px;
+        border-radius: 2px;
+        position: relative;
+        overflow: hidden;
+
+        .bg-effect {
+          width: 100%;
+          height: 100px;
+          border-radius: 2px;
+          background-size: cover;
+          background-repeat: no-repeat;
+          position: absolute;
+          transition: all 0.4s;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          overflow: hidden;
+
+          .layer {
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, .3);
+            position: absolute;
+            opacity: 1;
+            top: 0;
+            left: 0;
+            transition: opacity .3s ease-in-out;
+          }
+        }
+
+        .info {
+          padding: 10px;
+          color: #ffffff;
+          position: relative;
+          z-index: 10;
+
+          h3 {
+            font-size: 16px;
+
+            i {
+              font-size: 16px;
+              margin-top: -3px;
+              margin-right: 5px;
+            }
+          }
+
+          p {
+            margin-top: 5px;
+            font-size: 12px;
+          }
+        }
+      }
+
+      &:hover {
+        .bg-cover {
+          .bg-effect {
+            filter: blur(3px);
+            background-position: 90%;
+          }
+        }
+
+      }
+    }
+
+    .icon {
+      width: 30px;
+      margin-bottom: 10px;
+    }
+
+    h2 {
+      font-size: 28px;
+      font-weight: 700;
+    }
+
+    .desc {
+      color: #808695;
+      font-size: 12px;
+      margin-bottom: 10px;
+    }
+  }
+</style>
---
+++ src/components/home/todo/Index.vue
@@ -0,0 +1,95 @@
+<template>
+  <el-col :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
+    <div class="nice-card">
+      <div class="nice-card-header">
+        <span class="label flex-row"> <i class="iconfont nice-icon-flag"></i> 我的技能</span>
+      </div>
+      <div class="nice-card-bodyer">
+        <div class="todo-list">
+          <div class="item">
+            <p>HTML</p>
+            <el-tooltip class="item" effect="dark" content="动次打次" placement="bottom">
+              <el-progress :percentage="90" :stroke-width="10"></el-progress>
+            </el-tooltip>
+          </div>
+          <div class="item">
+            <p>CSS</p>
+            <el-tooltip class="item" effect="dark" content="苍茫的天涯是我的爱" placement="bottom">
+              <el-progress el-progress :percentage="90" :stroke-width="10" color="#000000" ></el-progress>
+            </el-tooltip>
+          </div>
+          <div class="item">
+            <p>JAVASCRIPT</p>
+            <el-tooltip class="item" effect="dark" content="绵绵的青山脚下花正开" placement="bottom">
+              <el-progress :percentage="70" :stroke-width="10" color="#11C26D" ></el-progress>
+            </el-tooltip>
+          </div>
+          <div class="item">
+            <p>VUE</p>
+            <el-tooltip class="item" effect="dark" content="什么样的节奏是最呀最摇摆" placement="bottom">
+              <el-progress :percentage="60" :stroke-width="10" color="#EB6709" ></el-progress>
+            </el-tooltip>
+          </div>
+          <div class="item">
+            <p>FLUTTER</p>
+            <el-tooltip class="item" effect="dark" content="什么样的歌声才是最开怀" placement="bottom">
+              <el-progress :percentage="50" :stroke-width="10" color="#909399" ></el-progress>
+            </el-tooltip>
+          </div>
+        </div>
+      </div>
+    </div>
+  </el-col>
+</template>
+
+<script>
+  import github from '@/utils/api.js'
+  export default {
+    data() {
+      return {
+        github
+      };
+    },
+    components: {},
+    computed: {},
+    watch: {},
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+  }
+</script>
+<style lang='scss' scoped>
+  .nice-card {
+    position: relative;
+
+    .nice-card-header {
+      .label {
+        flex: 1;
+
+        i {
+          font-size: 20px;
+          margin-right: 5px;
+        }
+      }
+    }
+
+    .nice-card-bodyer {
+      padding-top: 0;
+    }
+
+    .todo-list {
+      .item {
+        margin-bottom: 10px;
+        p {
+          margin-bottom: 10px;
+        }
+      }
+    }
+  }
+</style>
